f25d85229054e94b53974481ae2288d15e1076a2
ericmartinsm <ericmartins1997@gmail.com>
2021-05-10 17:52:17 -0300

    finishing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row class="justify-center">
+        <Sidebar class="col-3" style="background-color:#189AB4" />
+        <Body class="col-7" style="background-color:#D4F1F4"  />
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from "./components/sidebar";
+import Body from "./components/body";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Sidebar,
+    Body,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/body.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <v-card flat class="transparent">
+      <v-toolbar flat class="transparent">
+        <svg
+          width="25"
+          height="25"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="user"
+          class="svg-inline--fa fa-user fa-w-14 red rounded-circle pa-1"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
+          ></path>
+        </svg>
+        <v-toolbar-title class="ml-2"> Carrer Profile </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum</v-card-text
+      >
+
+      <!-- <v-card-title>Carrer Profile</v-card-title> -->
+    </v-card>
+    <v-card flat class="transparent">
+      <v-toolbar flat class="transparent">
+        <svg
+          width="25"
+          height="25"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="user"
+          class="svg-inline--fa fa-user fa-w-14 red rounded-circle pa-1"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
+          ></path>
+        </svg>
+        <v-toolbar-title class="ml-2"> Carrer Profile </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum</v-card-text
+      >
+
+      <!-- <v-card-title>Carrer Profile</v-card-title> -->
+    </v-card>
+    <v-card flat class="transparent">
+      <v-toolbar flat class="transparent">
+        <svg
+          width="25"
+          height="25"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="user"
+          class="svg-inline--fa fa-user fa-w-14 red rounded-circle pa-1"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
+          ></path>
+        </svg>
+        <v-toolbar-title class="ml-2"> Carrer Profile </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum</v-card-text
+      >
+
+      <!-- <v-card-title>Carrer Profile</v-card-title> -->
+    </v-card>
+    <v-card flat class="transparent">
+      <v-toolbar flat class="transparent">
+        <svg
+          width="25"
+          height="25"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="user"
+          class="svg-inline--fa fa-user fa-w-14 red rounded-circle pa-1"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
+          ></path>
+        </svg>
+        <v-toolbar-title class="ml-2"> Carrer Profile </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum</v-card-text
+      >
+
+      <!-- <v-card-title>Carrer Profile</v-card-title> -->
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
